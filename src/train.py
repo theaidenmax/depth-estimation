2ad46b831a43bde4f5e9d7b89c883fd685422a45
Aiden Max <305705548+theaidenmax@users.noreply.github.com>
2026-09-23 15:35:20 +0500

    Modify transform

--- src/train.py
+++ src/train.py
@@ -1,10 +1,11 @@
+from pathlib import Path
 import torch
 import torch.nn as nn
-import torchvision.transforms.v2 as v2
 from torch.utils.data import DataLoader, random_split
-from pathlib import Path
+import torchvision.transforms.v2 as v2
 
 from src.dataset import NYUDepthDataset
+from src.loss import SSIMLoss
 from src.models import get_depth_model
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -14,32 +15,31 @@ csv_path = Path("../data/nyu2_train_subset.csv")
 
 train_geom_transform = v2.Compose([
     v2.RandomHorizontalFlip(p=0.5),
-    v2.RandomResizedCrop(size=(256, 256), scale=(0.8, 1.0)),
+    v2.RandomResizedCrop(size=(384, 384), scale=(0.8, 1.0)),
 ])
 
 val_geom_transform = v2.Compose([
-    v2.Resize((256, 256)),
+    v2.Resize((384, 384)),
 ])
 
 train_img_transform = v2.Compose([
     v2.ColorJitter(brightness=0.2, contrast=0.2),
     v2.ToImage(),
     v2.ToDtype(torch.float32, scale=True),
-    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 val_img_transform = v2.Compose([
     v2.ToImage(),
     v2.ToDtype(torch.float32, scale=True),
-    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-depth_transform = v2.Compose([
-    v2.ToImage(),
-    v2.ToDtype(torch.float32, scale=True)
-])
+depth_transform = v2.Compose(
+    [v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]
+)
 
-dataset = NYUDepthDataset(csv_path, )
+dataset = NYUDepthDataset(csv_path)
 
 train_size = int(0.8 * len(dataset))
 val_size = len(dataset) - train_size
@@ -54,44 +54,72 @@ val_dataset.dataset.geom_transform = val_geom_transform
 val_dataset.dataset.img_transform = val_img_transform
 val_dataset.dataset.depth_transform = depth_transform
 
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, drop_last=True)
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=32,
+    shuffle=True,
+    drop_last=True,
+    num_workers=0,
+    pin_memory=True,
+)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 
 model = get_depth_model(pretrained=True).to(device)
 
-loss_fn = nn.L1Loss()
+depth_criterion = nn.SmoothL1Loss()
+ssim_criterion = SSIMLoss().to(device)
+
+ssim_weight = 0.05
 
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-n_epochs = 15
+n_epochs = 25
+
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+  optimizer, T_max=n_epochs, eta_min=1e-6
+)
 
 for param in model.parameters():
-    param.requires_grad = True
+  param.requires_grad = True
 
 for epoch in range(n_epochs):
-    model.train()
-    train_loss = 0.0
-    for images, depths in train_loader:
-        images, depths = images.to(device), depths.to(device)
+  model.train()
+  train_loss = 0.0
+  for images, depths in train_loader:
+    images, depths = images.to(device), depths.to(device)
 
-        optimizer.zero_grad()
-        preds = model(images)
-        loss = loss_fn(preds, depths)
-        loss.backward()
-        optimizer.step()
+    optimizer.zero_grad()
+    preds = model(images)
 
-        train_loss += loss.item()
+    l_depth = depth_criterion(preds, depths)
+    l_ssim = ssim_criterion(preds, depths)
+    loss = l_depth + (ssim_weight * l_ssim)
+    #print(f"l_depth: {l_depth}, l_ssim: {l_ssim}, loss: {loss}")
 
-    model.eval()
-    val_loss = 0.0
-    with torch.inference_mode():
-        for images, depths in val_loader:
-            images, depths = images.to(device), depths.to(device)
-            preds = model(images)
-            loss = loss_fn(preds, depths)
+    loss.backward()
+    optimizer.step()
 
-            val_loss += loss.item()
+    train_loss += loss.item()
 
-    print(f"Epoch [{epoch+1}/{n_epochs}] | "
-          f"Train Loss: {train_loss/len(train_loader):.4f} | "
-          f"Val Loss: {val_loss/len(val_loader):.4f}", flush=True)
+  model.eval()
+  val_loss = 0.0
+  with torch.inference_mode():
+    for images, depths in val_loader:
+      images, depths = images.to(device), depths.to(device)
+      preds = model(images)
+      l_depth = depth_criterion(preds, depths)
+      l_ssim = ssim_criterion(preds, depths)
+      loss = l_depth + (ssim_weight * l_ssim)
+
+      val_loss += loss.item()
+
+    scheduler.step()
+    current_lr = scheduler.get_last_lr()[0]
+
+
+    print(
+      f"Epoch [{epoch+1}/{n_epochs}] | LR: {current_lr:.6f} | "
+      f"Train Loss: {train_loss/len(train_loader):.4f} | "
+      f"Val Loss: {val_loss/len(val_loader):.4f}",
+      flush=True,
+  )
