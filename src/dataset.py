6cb56086b7244850d3509100629c07f159eeb77b
Aiden Max <305705548+theaidenmax@users.noreply.github.com>
2026-09-23 13:15:41 +0500

    Add dataset visualizing

--- src/dataset.py
+++ src/dataset.py
@@ -3,19 +3,38 @@ from PIL import Image
 import torch
 from torch.utils.data import Dataset
 import torchvision.transforms as T
+from pathlib import Path
 
 class NYUDepthDataset(Dataset):
     def __init__(self, csv_file, transform_img=None, transform_depth=None):
-        self.df = pd.read_csv(csv_file, header=None)
+        self.csv_path = Path(csv_file)
+        
+        self.root_dir = self.csv_path.parent 
+    
+        self.df = pd.read_csv(self.csv_path, header=None)
+        
         self.transform_img = transform_img
         self.transform_depth = transform_depth
 
     def __len__(self):
         return len(self.df)
 
+    def _fix_path(self, raw_path):
+        clean_path_str = str(raw_path).replace('\\', '/')
+        
+        path_obj = Path(clean_path_str)
+        if path_obj.parts[0] == 'data':
+            path_obj = Path(*path_obj.parts[1:])
+            
+        full_path = self.root_dir / path_obj
+        return full_path
+
     def __getitem__(self, idx):
-        img_path = self.df.iloc[idx, 0]
-        depth_path = self.df.iloc[idx, 1]
+        img_raw_path = self.df.iloc[idx, 0]
+        depth_raw_path = self.df.iloc[idx, 1]
+
+        img_path = self._fix_path(img_raw_path)
+        depth_path = self._fix_path(depth_raw_path)
 
         image = Image.open(img_path).convert("RGB")
         depth = Image.open(depth_path)
